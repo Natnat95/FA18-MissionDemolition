29bbce80cea7351b63b8b8377a7d37c66f0e46f1
Natasha Griffith <ng462@scarletmail.rutgers.edu>
2018-10-07 10:42:01 -0400

    Mission Demolition hw

---
+++ Assets/RigidbodySleep.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class RigidbodySleep : MonoBehaviour{
+    private void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null) rb.Sleep();
+    }
+}
